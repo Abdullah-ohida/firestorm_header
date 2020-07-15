7993217dfa7aef5448e1abf8605d93702377f7a7
Abdullah-ohida <ohida2001@gmail.com>
2020-07-15 07:13:13 +0100

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+const linksContainer = document.querySelector('.links-container')
+
+const date = document.querySelector(".date");
+date.innerHTML = new Date().getFullYear();
+
+navToggle.addEventListener('click', function(){
+    linksContainer.classList.toggle('show-links');
+})
